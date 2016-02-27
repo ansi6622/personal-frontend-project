4c86fe51d185e2e41d63b0035d4b3759325e70dc
Mickey <develop.agility@gmail.com>
2016-02-26 22:09:41 -0700

    house keeping

--- dev/components/Footer/index.js
+++ dev/components/Footer/index.js
@@ -1,18 +1,10 @@
 import React from "react";
 
-class Title extends React.Component {
-  render(){
-    return(
-      <h3>{this.props.title}</h3>
-    )
-  }
-}
-
 export default class Footer extends React.Component {
   render(){
     return(
       <footer>
-        <Title title={this.props.title}/>
+        Footer
       </footer>
     )
   }
