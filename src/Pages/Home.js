0caa68d0410bf1844fed79640c45285c96c42de1
Mickey <develop.agility@gmail.com>
2016-03-07 14:53:46 -0700

    another push to heroku

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
 import Orders from '../Modules/Orders/index';
-import Timer from '../Modules/Timer/index';
 
 export default class Home extends React.Component {
   render(){
     return(
       <div>
         <h3>Can I take ya ooorda?</h3>
-        <Timer initialCount={10}/>
         <Orders url='/api/orders' pollInterval={3000} />
       </div>
     )
--- src/Pages/Inventory.js
+++ src/Pages/Inventory.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import InventoryManager from '../Modules/Inventory/index';
 
 export default class Inventory extends React.Component{
@@ -6,7 +7,7 @@ export default class Inventory extends React.Component{
     return(
       <div>
         <h3>Inventory</h3>
-        <InventoryManager url="/items" pollInterval={3000}/>
+        <InventoryManager />
       </div>
     )
   }
